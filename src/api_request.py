6edf5f54c7f26ab733bedd2f106363affa365b01
Thomas Brockmeier <t.brockmeier@amsterdam.nl>
2022-04-20 13:53:01 +0200

    Fixes bug where fetched coordinates would be inverted when projecting to RD NEW
    Updates code to use medium size panorama images

--- src/api_request.py
+++ src/api_request.py
@@ -23,7 +23,7 @@ def get_pano_location(pano_id):
     
     # WGS84 to RD conversion 
     point = ogr.Geometry(ogr.wkbPoint)
-    point.AddPoint(geom[0], geom[1])
+    point.AddPoint(max(geom), min(geom))
     
     source = osr.SpatialReference()
     source.ImportFromEPSG(4326)
@@ -50,7 +50,7 @@ def get_pano_data(pano_id):
         return
     
     # Get panoramic image from API
-    image_url = pano_data['_links']['equirectangular_small']['href']
+    image_url = pano_data['_links']['equirectangular_medium']['href']
     
     # Get heading
     heading = pano_data['heading']
